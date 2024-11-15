aabc475994d0ba56f4c60bcadc9dfb602396a924
Shubham Niranjan <80174891+shubhamniranjan78@users.noreply.github.com>
2024-11-15 15:36:02 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,21 @@
+num1 = int(input("Enter a number : "))
+num2 = int(input("Enter another number : ")) 
+
+
+sum_of_num = num1 + num2
+
+diff_of_num = num1 - num2
+
+product_of_num = num1 * num2
+
+div_of_num = num1 % num2
+
+
+print("sum of 2 numbers : ",sum_of_num)
+
+print("difference of 2 numbers : ",diff_of_num)
+
+print("product of 2 numbers : ",product_of_num)
+
+print("division of 2 numbers : ",div_of_num)
+
